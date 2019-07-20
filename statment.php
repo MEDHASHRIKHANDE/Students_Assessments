109c72c0c4cb1ea3d7d5f0f23b36411861518b16
Mehtab7718Shaikh <51582690+Mehtab7718Shaikh@users.noreply.github.com>
2019-07-20 11:27:53 +0530

    Mehtab Shaikh

---
+++ statment.php
@@ -0,0 +1,29 @@
+1. What does PHP stand for?
+:- Both (i) and (ii)
+
+2. Who is the father of PHP?
+:- Rasmus Lerdorf
+
+3. PHP file have default file extension of?
+:- .PHP 
+
+4. A PHP script should with _____and with ___?
+:- <?php ?>
+
+5. Which of the looping statement is/are supported by PHP?
+:- All of the mentioned
+
+6. Which of the following is/are a PHP code editor?
+:- All of the mentioned
+
+7. Which of the following must be installedon your computer so as to run PHP script?
+:- (ii), (iii) and (iv)
+
+8. We can use to _ of to comment a single line?
+:- (ii), (iii) and (iv)
+
+9. Which of the below sysbols is a newline character?
+:- /n
+
+10. Which of the following php statement/statements will store 111 in variable num?
+:-  Only 3
